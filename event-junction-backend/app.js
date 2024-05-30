57245c56813b7786f4c72a79cad893c1a046508a
MdTamimAhamed <tamimssgts999@gmail.com>
2024-05-31 00:47:56 +0600

    Initialized the project

--- event-junction-backend/app.js
+++ event-junction-backend/app.js
@@ -1,37 +1,37 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const {
-  notFoundHandler,
-  errorHandler,
-} = require("./middlewares/common/errorHandler");
-const userRoutes = require("./routes/userRoutes");
-const adminRoutes = require("./routes/adminRoutes");
+	notFoundHandler,
+	errorHandler,
+} = require('./middlewares/common/errorHandler');
+const userRoutes = require('./routes/userRoutes');
+const adminRoutes = require('./routes/adminRoutes');
 
 const app = express();
 dotenv.config();
 
 //database connection
 const db = mongoose
-  .connect(process.env.MONGOOSE_CONNECTION_STRING, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Database connection successfull!");
-  })
-  .catch((err) => console.log(err));
+	.connect(process.env.MONGOOSE_CONNECTION_STRING, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		console.log('Database connection successfull!');
+	})
+	.catch((err) => console.log(err));
 
 //middlewares
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 //routes
-app.use("/user", userRoutes);
-app.use("/admin", adminRoutes);
+app.use('/user', userRoutes);
+app.use('/admin', adminRoutes);
 
 //error handlers
 app.use(notFoundHandler); //404 handler
@@ -39,5 +39,5 @@ app.use(errorHandler); //default error handler
 
 //listen to port
 app.listen(process.env.PORT, () => {
-  console.log(`Server running on port: ${process.env.PORT}`);
+	console.log(`Server running on port: ${process.env.PORT}`);
 });
--- event-junction-backend/routes/userRoutes.js
+++ event-junction-backend/routes/userRoutes.js
@@ -1,27 +1,27 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const { signupUser, loginUser } = require("../controller/userController");
+const { signupUser, loginUser } = require('../controller/userController');
 const {
-  userSignupValidators,
-  userSignupValidatorsErrorHandler,
-} = require("../validators/signupValidator");
+	userSignupValidators,
+	userSignupValidatorsErrorHandler,
+} = require('../validators/signupValidator');
 
 const {
-  userLoginValidators,
-  userLoginValidatorErrorHandler,
-} = require("../validators/loginValidator");
+	userLoginValidators,
+	userLoginValidatorErrorHandler,
+} = require('../validators/loginValidator');
 
 router.post(
-  "/signup",
-  userSignupValidators,
-  userSignupValidatorsErrorHandler,
-  signupUser
+	'/signup',
+	userSignupValidators,
+	userSignupValidatorsErrorHandler,
+	signupUser
 );
 router.post(
-  "/login",
-  userLoginValidators,
-  userLoginValidatorErrorHandler,
-  loginUser
+	'/login',
+	userLoginValidators,
+	userLoginValidatorErrorHandler,
+	loginUser
 );
 
 module.exports = router;
